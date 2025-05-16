06dbdfa3b5d4f4e6d2767759d5f1887a65b61b25
sujonbiswas212 <wwwsujonbiswas212@gmail.com>
2025-05-17 02:01:19 +0600

    fist

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+export default defineConfig({
+     build:{
+          rollupOptions:{
+               main:resolve(__dirname,"index.html"),
+               news:resolve(__dirname,"news.html"),
+          },
+     },
+});
